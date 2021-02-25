107fbea0eb2bb48231e8dac9d9217f29410e278c
Dakota Upchurch <cheyenneupchurch@hotmail.com>
2021-02-25 14:36:22 -0600

    Added styling in css, established base html, and started working on adding fish objects

---
+++ Scripts/main.js
@@ -0,0 +1,23 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "crustaceans",
+        inches: 16,
+        species: "Baracuda",
+        harvestLocation: "Indian Sea"
+        image: (img src="./images")
+    }
+    {
+        name: "Lisa",
+        food: "small fish",
+        inches: 56,
+        species: "Bull Shark",
+        harvestLocation: "South Florida"
+    }
+    {
+        name: "Homer",
+        food: "donuts",
+        inches: 44,
+        species: "Beluga Whale"
+    }
+]
